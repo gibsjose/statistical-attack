4b5d209b6a1a1e0f12d2d41151290f0c879f6a37
Joe Gibson <joseph.gibson@nasa.gov>
2016-05-11 20:22:33 -0400

    Improve statistical attack

--- statistical-attack.py
+++ statistical-attack.py
@@ -17,13 +17,54 @@ from collections import deque
 from colorama import Fore, Back, Style
 import random
 
+TOTAL_CHARACTERS = 4374127904
+
+MONOGRAM_FILE = 'english-monograms.txt'
+BIGRAM_FILE = 'english-bigrams.txt'
+TRIGRAM_FILE = 'english-trigrams.txt'
+QUADGRAM_FILE = 'english-quadgrams.txt'
+
 class Statistics:
     """Statistics"""
     
-    def __init__(self, ciphertext):
+    def __init__(self, ciphertext, alphabet):
         """Constructor"""
         self.ciphertext = ciphertext
-     
+        self.alphabet = alphabet
+        self.frequency_distribution = {}
+        self.letter_count = {}
+        
+        self.DetermineLetterCount()
+        self.DetermineFrequencyDistribution()
+    
+    def DetermineFrequencyDistribution(self):
+        """Determine the frequency distribution of the ciphertext"""
+        for letter in self.letter_count:
+            self.frequency_distribution[letter] = (self.letter_count[letter] / self.total) * 100
+        
+        # Fill in gaps
+        for letter in self.alphabet:
+            if letter not in self.frequency_distribution.keys():
+                self.frequency_distribution[letter] = 0
+        
+        print(self.frequency_distribution)
+                
+    def DetermineLetterCount(self):
+        """Determine the count of each letter and the total count"""
+        self.total = 0
+        
+        for letter in self.ciphertext:
+            if letter in self.alphabet:
+                if letter in self.letter_count.keys():
+                    self.letter_count[letter] += 1
+                else:
+                    self.letter_count[letter] = 1
+                
+                self.total += 1
+                
+    def GetFrequencyDistribution(self):
+        """Get frequency distribution"""
+        return self.frequency_distribution
 
 class StatisticalAttack:
     """Statistical Attack"""
@@ -31,11 +72,26 @@ class StatisticalAttack:
     def __init__(self, arguments):
         """Constructor"""        
         self.ciphertext_file = arguments['<ciphertext>']
-        self.depth = arguments['--depth']
+        self.depth = int(arguments['--depth'])
         
         self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
+        self.ReadNGram()
+        
+    def ReadNGram(self):
+        """Read the correct N-gram file"""
+        self.frequency_distribution = {}
+                
+        # Monogram
+        if self.depth == 1:
+            with open(MONOGRAM_FILE, 'r') as mf:
+                for line in mf.readlines():
+                    tokens = line.split()
+                    key = str(tokens[0]).lower()
+                    frequency = (int(tokens[1]) / TOTAL_CHARACTERS) * 100
+                    self.frequency_distribution[key] = frequency
+                            
     def ReadCiphertext(self):
         """Read ciphertext file into string"""
         with open(self.ciphertext_file, 'r') as ctf:
@@ -43,11 +99,17 @@ class StatisticalAttack:
             
     def Analyze(self):
         """Analyze ciphertext"""
-        self.statistics = Statistics(self.ciphertext)
+        self.statistics = Statistics(self.ciphertext, self.alphabet)
     
     def GenerateKey(self):
         """Determine the key from the ciphertext analysis"""
-        self.key = list(self.alphabet)
+        # Ciphertext frequency distribution
+        cfd = self.statistics.frequency_distribution
+        
+        # Average frequency distribution
+        afd = self.frequency_distribution
+        
+        
     
     def GenerateKeymap(self):
         """Generate Keymap"""
